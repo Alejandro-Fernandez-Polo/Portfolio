f289536f1d82e098866f15bfd00b611ac3140193
Alejandro Fernandez Polo <afernanpolo@gmail.com>
2025-09-19 20:18:48 +0200

    Update section styles in Home component for improved layout and responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ export function Home() {
   const [planeScale, planePosition] = adjustPlaneForScreenSize()
 
   return (
-    <section className="w-full h-[100dhv] relative">
+    <section className="overflow-hidden w-full h-screen relative">
       <div
         className="absolute top-28 left-0 right-0 z-10 flex
       items-center justify-center"
