84cedac44582f254bb2bb8fbfa25dd1016e5b109
Alejandro Fernandez Polo <afernanpolo@gmail.com>
2025-09-22 20:22:25 +0200

    Add vercer analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { Navbar } from "./components/Navbar"
 import { Footer } from "./components/Footer"
 import { Home, About, Projects, Contact } from "./pages"
 import "./libs/i18n/i18n"; // importante para inicializar i18n
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <main className="background">
+      <Analytics />
       <Router>
         <Navbar />
         <Routes>
