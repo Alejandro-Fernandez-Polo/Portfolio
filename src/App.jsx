1aa144a737c0710c417fdbe0f712819171518484
Alejandro Fernandez Polo <afernanpolo@gmail.com>
2025-09-22 20:27:19 +0200

    Add vercel speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { Footer } from "./components/Footer"
 import { Home, About, Projects, Contact } from "./pages"
 import "./libs/i18n/i18n"; // importante para inicializar i18n
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
     <main className="background">
       <Analytics />
+      <SpeedInsights />
       <Router>
         <Navbar />
         <Routes>
