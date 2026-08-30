8b81e3b5af554be920f7242691ea102ab96a4449
Alejandro Fernandez Polo <afernanpolo@gmail.com>
2026-08-30 15:59:43 +0200

    Fix paths in HTML and Vite config for consistency; ensure correct loading of styles and scripts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import fs from "fs"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "ugr-page",
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.match(/^\/ugr\/?$/)) {
+            req.url = "/ugr/index.html"
+          }
+          next()
+        })
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.match(/^\/ugr\/?$/)) {
+            req.url = "/ugr/index.html"
+          }
+          next()
+        })
+      },
+    },
+  ],
 })
